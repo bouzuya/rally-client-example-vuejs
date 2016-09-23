2d2da207e2724daad350879939b11e9b3c282b23
bouzuya <m@bouzuya.net>
2016-09-23 14:25:37 +0900

    add dummy #app element

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import add from './add';
+import * as Vue from 'vue';
 
-export default function main() {
-  return add(3, 4);
-}
+const main = (): void => {
+  type Data = { count: number; };
+  const data: Data = { count: 0 };
+  const vue = new Vue({
+    el: '#app',
+    data,
+    computed: {
+      message(this: Data): string {
+        return `count = ${this.count}`;
+      }
+    },
+    methods: {
+      click(this: Data): void {
+        this.count += 1;
+      }
+    }
+  });
+  console.log(vue);
+};
+
+const ready = (callback: Function): void => {
+  if (typeof document === 'undefined') return void callback();
+  if (document.readyState === 'complete') {
+    setTimeout(() => callback());
+  } else {
+    document.addEventListener('DOMContentLoaded', function listener() {
+      document.removeEventListener('DOMContentLoaded', listener);
+      callback();
+    });
+  }
+};
+
+ready(main);
