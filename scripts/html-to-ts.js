58b9266d417e14e24f1d3bde6941de447919e4ea
bouzuya <m@bouzuya.net>
2016-09-25 08:35:12 +0900

    fix build script bug

--- scripts/html-to-ts.js
+++ scripts/html-to-ts.js
@@ -4,25 +4,27 @@ const path = require('path');
 
 const formatContent = (s) => `const template = '${s}'; export { template };`;
 const formatFile = (f) => {
-  const removeExtension = (f) => {
+  const removeExtension = (f, dst) => {
     const l = path.extname(f).length;
     return f.slice(0, f.length - l);
   };
-  return removeExtension(f) + '.ts';
+  const p = removeExtension(f) + '.ts';
+  return path.join(dst, path.basename(p));
 };
 
 const load = (f) => fs.readFileSync(f, { encoding: 'utf-8' });
 const save = (f, d) => fs.writeFileSync(f, d, { encodint: 'utf-8' });
-const htmlToTs = (of) => {
+const htmlToTs = (of, dir) => {
   const oc = load(of);
-  const nf = formatFile(of);
+  const nf = formatFile(of, dst);
   const nc = formatContent(jsStringEscape(oc));
   save(nf, nc);
 };
 
-const dir = process.argv[2];
-fs.readdirSync(dir).forEach((file) => {
+const src = process.argv[2];
+const dst = process.argv[3];
+fs.readdirSync(src).forEach((file) => {
   if (file.match(/\.html/)) {
-    htmlToTs(path.join(dir, file));
+    htmlToTs(path.join(src, file), dst);
   }
 });
