47f3472fd87816ba2097bb387febd6859c45775b
bouzuya <m@bouzuya.net>
2016-09-24 17:52:44 +0900

    extract templates/stamp-rally.bhtml

--- src/views/stamp-rally.ts
+++ src/views/stamp-rally.ts
@@ -1,5 +1,6 @@
 import { Spot } from '../types/spot';
 import { StampRally } from '../types/stamp-rally';
+import { template } from '../templates/stamp-rally';
 
 type StampRallyData = {
   stampRally: StampRally | null;
@@ -24,22 +25,7 @@ const view = {
     }
   },
   props: ['stamp-rally'],
-  template: `
-<div class="stamp-rally">
-  <div class="name" v-text="stampRallyName"></div>
-  <div class="tagline" v-html="stampRallyTagline"></div>
-  <div class="description" v-html="stampRallyDescription"></div>
-  <div class="image"><img width="64" height="64" v-bind:src="stampRallyImage" /></div>
-  <ul>
-    <li v-for="spot in stampRallySpots">
-      <div class="name" v-text="spot.name"></div>
-      <div class="tagline" v-html="spot.tagline"></div>
-      <div class="description" v-html="spot.description"></div>
-      <div class="image"><img width="64" height="64" v-bind:src="spot.image" /></div>
-    </li>
-  </ul>
-</div>
-`
+  template
 };
 
 export { view };
