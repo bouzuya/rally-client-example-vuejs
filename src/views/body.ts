5d9ae39355d7b8854ed32a47ef3d38fa2f486a48
bouzuya <m@bouzuya.net>
2016-10-02 07:41:20 +0900

    apply bus to views/body

--- src/views/body.ts
+++ src/views/body.ts
@@ -1,85 +1,12 @@
-import { create, RallyClient } from 'rally-client';
-import { createStamp } from '../create-stamp';
-import { ensureStampCard, StampCard } from '../ensure-stamp-card';
-import { ensureUser } from '../ensure-user';
-import { getLocation } from '../get-location';
 import { Spot } from '../types/spot';
 import { StampRally } from '../types/stamp-rally';
 import { view as SpotView } from '../views/spot';
 import { view as StampRallyView } from '../views/stamp-rally';
 import { template } from '../views/templates/body';
 import { MessageBus } from '../message-bus';
-
-interface ApiSpot {
-  id: number;
-  name: string;
-  tagline: string | null;
-  description: string | null;
-  images: { s640: string; }[];
-  lat: string;
-  lng: string;
-  stampByLocation: boolean;
-}
-
-interface ApiStampRally {
-  name: string;
-  displayName: string;
-  tagline: string | null;
-  description: string | null;
-  spots: ApiSpot[];
-  images: { s640: string; }[];
-}
-
-const getStampRally = (
-  client: RallyClient, stampRallyId: string
-): Promise<StampRally> => {
-  return client.call({
-    method: 'GET',
-    path: `/rallies/${stampRallyId}`
-  }).then((rally: ApiStampRally) => {
-    const id = rally.name;
-    const name = rally.displayName;
-    const taglineHtml = rally.tagline;
-    const descriptionHtml = rally.description;
-    const spots = rally.spots;
-    const image = rally.images.length > 0 ? rally.images[0].s640 : null;
-    return {
-      id,
-      name,
-      tagline: taglineHtml === null ? '' : taglineHtml,
-      description: descriptionHtml === null ? '' : descriptionHtml,
-      image,
-      spots: spots.map((spot) => {
-        const id = spot.id;
-        const name = spot.name;
-        const taglineHtml = spot.tagline;
-        const descriptionHtml = spot.description;
-        const image = spot.images.length > 0 ? spot.images[0].s640 : null;
-        const lat = +spot.lat;
-        const lng = +spot.lng;
-        const stampByLocation = spot.stampByLocation;
-        return {
-          id,
-          name,
-          tagline: taglineHtml === null ? '' : taglineHtml,
-          description: descriptionHtml === null ? '' : descriptionHtml,
-          image,
-          lat,
-          lng,
-          stampByLocation
-        };
-      })
-    };
-  });
-};
-
-const getStampCard = (
-  client: RallyClient, stampRallyId: string
-): Promise<StampCard> => {
-  return ensureUser(client).then((user) => {
-    return ensureStampCard(client, stampRallyId, user.id);
-  });
-};
+import { createStampCommand, createSwitchCommand } from '../message';
+import { StampCard } from '../ensure-stamp-card';
+import { connect } from './utils/connect';
 
 export interface Props {
   bus: MessageBus;
@@ -87,7 +14,6 @@ export interface Props {
 }
 
 export interface State {
-  client: RallyClient;
   stampCard: StampCard | null;
   stampRally: StampRally | null;
 }
@@ -109,7 +35,6 @@ const view = {
   },
   data(): State {
     return {
-      client: create(),
       stampCard: null,
       stampRally: null
     };
@@ -123,36 +48,10 @@ const view = {
       return this.stampCard.spots.some((s) => s.id === spot.id && s.stamped);
     },
     stamp(this: Props & State, spotId: number): void {
-      Promise.all([
-        getStampCard(this.client, this.stampRallyId),
-        getLocation()
-      ])
-        .then(([stampCard, { lat, lng }]) => {
-          return createStamp(this.client, stampCard.id, spotId, lat, lng);
-        })
-        .then(() => {
-          // reload stamp card
-          return getStampCard(this.client, this.stampRallyId);
-        })
-        .then((stampCard) => {
-          this.stampCard = stampCard;
-        });
+      this.bus.publish(createStampCommand(spotId));
     },
     switch(this: Props & State, stampRallyId: string): void {
-      this.stampCard = null;
-      this.stampRally = null;
-      if (this.stampCard === null) {
-        getStampCard(this.client, stampRallyId)
-          .then((stampCard) => {
-            this.stampCard = stampCard;
-          });
-      }
-      if (this.stampRally === null) {
-        getStampRally(this.client, stampRallyId)
-          .then((stampRally) => {
-            this.stampRally = stampRally;
-          });
-      }
+      this.bus.publish(createSwitchCommand(stampRallyId));
     }
   },
   template,
@@ -163,4 +62,9 @@ const view = {
   }
 };
 
-export { view };
+const connectedView = connect(view, (state) => {
+  const { stampCard, stampRally } = state;
+  return { stampCard, stampRally };
+});
+
+export { connectedView as view };
